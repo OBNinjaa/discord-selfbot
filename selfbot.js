eb56b2afd6ce187ec5984af7d05c50ee91ceea78
OBNinjaa <64389208+OBNinjaa@users.noreply.github.com>
2021-01-28 06:00:12 +0000

    Status
    Added mobile status indicator.

--- selfbot.js
+++ selfbot.js
@@ -2,6 +2,9 @@ const Discord = require("discord.js");
 var config = require("./config.json");
 const fs = require("fs");
 
+const Constants = require("discord.js/src/util/Constants.js");
+Constants.DefaultOptions.ws.properties.$browser = `Discord iOS`;
+
 const client = new Discord.Client();
 const { prefix, token, sbversion } = config;
 client.on("ready", () => {
@@ -10,6 +13,7 @@ client.on("ready", () => {
 	console.log("[CLIENT] Username: " + client.user.username);
 	console.log("[CLIENT] Running on Discord API version " + Discord.version);
 	console.log("[CLIENT] Running on Selfbot Name version " + sbversion);
+	client.user.setActivity(`videos & chilling`, { type: 3, browser: "DISCORD IOS" });
 });
 
 client.login(token);
