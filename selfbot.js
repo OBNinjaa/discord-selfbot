a0c514985f8bddb4be6a253f4f032d2f09e6955d
OBNinjaa <64389208+OBNinjaa@users.noreply.github.com>
2021-01-30 07:09:50 +0000

    Update selfbot.js

--- selfbot.js
+++ selfbot.js
@@ -6,24 +6,30 @@ const Constants = require("discord.js/src/util/Constants.js");
 Constants.DefaultOptions.ws.properties.$browser = `Discord iOS`;
 
 const client = new Discord.Client();
-const { prefix, token, sbversion } = config;
+const {
+    prefix,
+    token,
+    sbversion
+} = config;
 client.on("ready", () => {
-	client.login(config.token);
-	console.log("[CLIENT] Connected to Discord via the token successfully.");
-	console.log("[CLIENT] Username: " + client.user.username);
-	console.log("[CLIENT] Running on Discord API version " + Discord.version);
-	console.log("[CLIENT] Running on Selfbot Name version " + sbversion);
-	client.user.setActivity(`Made by OBNinjaa`, { type: 3, browser: "DISCORD IOS" });
+    client.login(config.token);
+    console.log("[CLIENT] Connected to Discord via the token successfully.");
+    console.log("[CLIENT] Username: " + client.user.username);
+    console.log("[CLIENT] Running on Discord API version " + Discord.version);
+    console.log("[CLIENT] Running on Selfbot Name version " + sbversion);
+    client.user.setActivity(`Made by OBNinjaa`, {
+        type: 3,
+        browser: "DISCORD IOS"
+    });
 });
 
-client.on("message", function (message) {
-	if (message.content == ".bump") {
-		message.channel.send("**Auto bumper has started!**");
-
-		var interval = setInterval(function () {
-			message.channel.send("!bump");
-		}, 7.2e6);
-	}
+client.on("message", function(message) {
+    if (message.content == ".bump") {
+        message.channel.send("**Auto bumper has started!**");
+        var interval = setInterval(function() {
+            message.channel.send("!bump");
+        }, 7.2e6);
+    }
 });
 
 client.login(token);
