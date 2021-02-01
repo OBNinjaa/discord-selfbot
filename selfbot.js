21cdc3e0dbcce40d87bb3a98f980a3155232e6a6
OBNinjaa <64389208+OBNinjaa@users.noreply.github.com>
2021-02-01 11:01:49 +0000

    Update selfbot.js

--- selfbot.js
+++ selfbot.js
@@ -1,35 +1,73 @@
 const Discord = require("discord.js");
+const client = new Discord.Client();
 var config = require("./config.json");
+const chalk = require("chalk");
+const log = console.log;
 const fs = require("fs");
+process.title = "discord.gg/wolkig";
 
+const { owner, prefix, token, sbversion, advert, response } = config;
 const Constants = require("discord.js/src/util/Constants.js");
 Constants.DefaultOptions.ws.properties.$browser = `Discord iOS`;
 
-const client = new Discord.Client();
-const {
-    prefix,
-    token,
-    sbversion
-} = config;
 client.on("ready", () => {
-    client.login(config.token);
-    console.log("[CLIENT] Connected to Discord via the token successfully.");
-    console.log("[CLIENT] Username: " + client.user.username);
-    console.log("[CLIENT] Running on Discord API version " + Discord.version);
-    console.log("[CLIENT] Running on Selfbot Name version " + sbversion);
-    client.user.setActivity(`Made by OBNinjaa`, {
-        type: 3,
-        browser: "DISCORD IOS"
-    });
+	client.login(config.token);
+	log(chalk.cyan("[CLIENT] Connected to Discord via the token successfully."));
+	log(chalk.cyan("[CLIENT] Logged in as: " + client.user.username));
+	log(chalk.cyan("[CLIENT] Running on Discord API version " + Discord.version));
+	log(chalk.cyan("[CLIENT] Running on Selfbot Name version " + sbversion));
+	client.user.setActivity(``, { type: 0, browser: "DISCORD IOS" });
 });
 
-client.on("message", function(message) {
-    if (message.content == ".bump") {
-        message.channel.send("**Auto bumper has started!**");
-        var interval = setInterval(function() {
-            message.channel.send("!bump");
-        }, 7.2e6);
-    }
+// Auto bump - Disboard
+client.on("message", function (message) {
+	if (message.content == ".bump") {
+		message.channel.send("**Disboard auto bump started!**");
+
+		var interval = setInterval(function () {
+			message.channel.send("!bump");
+		}, 7.2e6); // Sends the message every 20 minuets
+	}
 });
 
-client.login(token);
+// Auto Beg - Dank Memer
+client.on("message", function (message) {
+	if (message.content == ".beg") {
+		message.channel.send("**Dank Memer auto beg started!**");
+
+		var interval = setInterval(function () {
+			message.channel.send("pls beg");
+		}, 45000); // Sends the message every 45 seconds
+	}
+});
+
+// Auto advertisement
+client.on("message", function (message) {
+	if (message.content == ".advertise") {
+		message.channel.send("**Advertisement process started!**");
+
+		var interval = setInterval(function () {
+			message.channel.send(advert);
+		}, 3.6e6); // Sends the message every hour
+	}
+});
+
+// Help
+client.on("message", function (message) {
+	if (message.content == ".help") {
+		const exampleEmbed = new Discord.RichEmbed()
+			.setColor("#ff0000")
+			.setTitle("**List Of Commands**")
+			.setThumbnail("https://i.imgur.com/kP05HKu.png")
+			.addField(".bump", "Automatically bumps your guild")
+			.addField(".beg", "Automatically begs (Requires Dank Memer)")
+			.addField(".advertise", "Sends an advertisement every hour")
+
+			.setTimestamp()
+			.setFooter("Created by OBNinjaa", "https://i.imgur.com/kP05HKu.png");
+
+		message.channel.send(exampleEmbed);
+	}
+});
+
+https: client.login(token);
