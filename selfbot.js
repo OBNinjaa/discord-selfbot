1be841d88bb1463ea62c3ba1bb2d663ca882edc2
OBNinjaa <64389208+OBNinjaa@users.noreply.github.com>
2021-02-02 14:00:19 +0000

    Update selfbot.js

--- selfbot.js
+++ selfbot.js
@@ -6,7 +6,7 @@ const log = console.log;
 const fs = require("fs");
 process.title = "discord.gg/wolkig";
 
-const { owner, prefix, token, sbversion, advert, response } = config;
+const { prefix, token, sbversion } = config;
 const Constants = require("discord.js/src/util/Constants.js");
 Constants.DefaultOptions.ws.properties.$browser = `Discord iOS`;
 
@@ -19,39 +19,6 @@ client.on("ready", () => {
 	client.user.setActivity(``, { type: 0, browser: "DISCORD IOS" });
 });
 
-// Auto bump - Disboard
-client.on("message", function (message) {
-	if (message.content == ".bump") {
-		message.channel.send("**Disboard auto bump started!**");
-
-		var interval = setInterval(function () {
-			message.channel.send("!bump");
-		}, 7.2e6); // Sends the message every 20 minuets
-	}
-});
-
-// Auto Beg - Dank Memer
-client.on("message", function (message) {
-	if (message.content == ".beg") {
-		message.channel.send("**Dank Memer auto beg started!**");
-
-		var interval = setInterval(function () {
-			message.channel.send("pls beg");
-		}, 45000); // Sends the message every 45 seconds
-	}
-});
-
-// Auto advertisement
-client.on("message", function (message) {
-	if (message.content == ".advertise") {
-		message.channel.send("**Advertisement process started!**");
-
-		var interval = setInterval(function () {
-			message.channel.send(advert);
-		}, 3.6e6); // Sends the message every hour
-	}
-});
-
 // Help
 client.on("message", function (message) {
 	if (message.content == ".help") {
@@ -59,9 +26,13 @@ client.on("message", function (message) {
 			.setColor("#ff0000")
 			.setTitle("**List Of Commands**")
 			.setThumbnail("https://i.imgur.com/kP05HKu.png")
-			.addField(".bump", "Automatically bumps your guild")
-			.addField(".beg", "Automatically begs (Requires Dank Memer)")
-			.addField(".advertise", "Sends an advertisement every hour")
+			.addField("command", "description")
+			.addField("command", "description")
+			.addField("command", "description")
+			.addField("command", "description")
+			.addField("command", "description")
+			.addField("command", "description")
+			.addField("command", "description")
 
 			.setTimestamp()
 			.setFooter("Created by OBNinjaa", "https://i.imgur.com/kP05HKu.png");
@@ -70,4 +41,4 @@ client.on("message", function (message) {
 	}
 });
 
-https: client.login(token);
+client.login(token);
