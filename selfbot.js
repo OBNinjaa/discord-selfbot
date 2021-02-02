a7e14006c6387fc593705aef001f93628aef7529
OBNinjaa <64389208+OBNinjaa@users.noreply.github.com>
2021-02-02 14:01:09 +0000

    Update selfbot.js

--- selfbot.js
+++ selfbot.js
@@ -6,39 +6,46 @@ const log = console.log;
 const fs = require("fs");
 process.title = "discord.gg/wolkig";
 
-const { prefix, token, sbversion } = config;
+const {
+    prefix,
+    token,
+    sbversion
+} = config;
 const Constants = require("discord.js/src/util/Constants.js");
 Constants.DefaultOptions.ws.properties.$browser = `Discord iOS`;
 
 client.on("ready", () => {
-	client.login(config.token);
-	log(chalk.cyan("[CLIENT] Connected to Discord via the token successfully."));
-	log(chalk.cyan("[CLIENT] Logged in as: " + client.user.username));
-	log(chalk.cyan("[CLIENT] Running on Discord API version " + Discord.version));
-	log(chalk.cyan("[CLIENT] Running on Selfbot Name version " + sbversion));
-	client.user.setActivity(``, { type: 0, browser: "DISCORD IOS" });
+    client.login(config.token);
+    log(chalk.cyan("[CLIENT] Connected to Discord via the token successfully."));
+    log(chalk.cyan("[CLIENT] Logged in as: " + client.user.username));
+    log(chalk.cyan("[CLIENT] Running on Discord API version " + Discord.version));
+    log(chalk.cyan("[CLIENT] Running on Selfbot Name version " + sbversion));
+    client.user.setActivity(``, {
+        type: 0,
+        browser: "DISCORD IOS"
+    });
 });
 
 // Help
-client.on("message", function (message) {
-	if (message.content == ".help") {
-		const exampleEmbed = new Discord.RichEmbed()
-			.setColor("#ff0000")
-			.setTitle("**List Of Commands**")
-			.setThumbnail("https://i.imgur.com/kP05HKu.png")
-			.addField("command", "description")
-			.addField("command", "description")
-			.addField("command", "description")
-			.addField("command", "description")
-			.addField("command", "description")
-			.addField("command", "description")
-			.addField("command", "description")
+client.on("message", function(message) {
+    if (message.content == ".help") {
+        const exampleEmbed = new Discord.RichEmbed()
+            .setColor("#ff0000")
+            .setTitle("**List Of Commands**")
+            .setThumbnail("https://i.imgur.com/kP05HKu.png")
+            .addField("command", "description")
+            .addField("command", "description")
+            .addField("command", "description")
+            .addField("command", "description")
+            .addField("command", "description")
+            .addField("command", "description")
+            .addField("command", "description")
 
-			.setTimestamp()
-			.setFooter("Created by OBNinjaa", "https://i.imgur.com/kP05HKu.png");
+            .setTimestamp()
+            .setFooter("Created by OBNinjaa", "https://i.imgur.com/kP05HKu.png");
 
-		message.channel.send(exampleEmbed);
-	}
+        message.channel.send(exampleEmbed);
+    }
 });
 
 client.login(token);
